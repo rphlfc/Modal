fc6f7eaee913fbb0ea1f77a2e429aba6190801eb
Raphael Cerqueira <raphaelfc@gmail.com>
2021-05-06 18:26:15 -0300

    Initial Commit

---
+++ Modal/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Modal
+//
+//  Created by Raphael Cerqueira on 06/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Modal/ModalApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModalApp.swift
+//  Modal
+//
+//  Created by Raphael Cerqueira on 06/05/21.
+//
+
+import SwiftUI
+
+@main
+struct ModalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
