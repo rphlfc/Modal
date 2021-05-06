47dd4595204bdbd0961efbd29d7ec5b2f31939d0
Raphael Cerqueira <raphaelfc@gmail.com>
2021-05-06 18:51:03 -0300

    Primeiro commit

--- Modal/ContentView.swift
+++ Modal/ContentView.swift
@@ -8,14 +8,90 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isModalVisible = false
+    
     var body: some View {
-        Text("Hello, world!")
+        ZStack {
+            Button(action: {
+                isModalVisible.toggle()
+            }, label: {
+                Text("Show modal")
+                    .frame(width: 120, height: 60)
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(8)
+            })
+            
+            if isModalVisible {
+                ModalView(isVisible: $isModalVisible)
+            }
+        }
+    }
+}
+
+struct ModalView: View {
+    @Binding var isVisible: Bool
+    
+    var body: some View {
+        ZStack {
+            Color.primary.opacity(0.3)
+                .ignoresSafeArea()
+            
+            VStack(spacing: 20) {
+                HStack {
+                    Spacer()
+                    
+                    Button(action: {
+                        toggleVisible()
+                    }, label: {
+                        Image(systemName: "xmark")
+                            .foregroundColor(.primary)
+                            .padding(8)
+                            .background(Color(UIColor.systemBackground))
+                            .clipShape(Circle())
+                            .shadow(radius: 3)
+                    })
+                }
+                
+                Image(systemName: "hand.thumbsup")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 50)
+                
+                Text("Payment\ncompleted")
+                    .font(.title3)
+                    .fontWeight(.bold)
+                    .multilineTextAlignment(.center)
+                
+                Text("Congratulations! Your payment has been successfully completed.")
+                    .multilineTextAlignment(.center)
+                
+                Button(action: {
+                    toggleVisible()
+                }, label: {
+                    Text("Finish")
+                        .foregroundColor(Color(UIColor.systemBackground))
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 60)
+                        .background(Color.primary)
+                        .cornerRadius(15)
+                })
+            }
             .padding()
+            .background(Color(UIColor.systemBackground))
+            .cornerRadius(15)
+            .padding()
+        }
+    }
+    
+    func toggleVisible() {
+        isVisible.toggle()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
     }
 }
---
+++ README.md
@@ -0,0 +1,25 @@
+# Modal usando SwiftUI 
+Encontrei esse [post no Instagram](https://www.instagram.com/p/CN2A5lkIwO6/) sobre como tornar seus modais mais amigáveis e implementei usando SwiftUI
+
+## Screenshots
+<p align="center">
+    <img src="https://user-images.githubusercontent.com/16376748/117369719-b123a800-ae9b-11eb-9577-e5acc7d41587.png" width="400"> &nbsp;
+</p>
+
+## Vídeos
+- [Vídeo no YouTube](https://youtu.be/i3502JZGotk)
+
+## Requisitos
+- iOS 14
+- SwiftUI
+- Xcode 12
+
+## Meta
+- Github: [https://github.com/rphlfc](https://github.com/rphlfc)
+- Instagram: [https://www.instagram.com/rphlfc.dev](https://www.instagram.com/rphlfc.dev)
+- LinkedIn: [http://www.linkedin.com/in/rphlfc](http://www.linkedin.com/in/rphlfc)
+- Twitter: [https://twitter.com/rphlfc](https://twitter.com/rphlfc)
+- Website: [https://rphlfc.github.io](https://rphlfc.github.io)
+- Youtube: [https://www.youtube.com/c/raphaelcerqueira](https://www.youtube.com/c/raphaelcerqueira)
+
+
